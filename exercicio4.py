c8f4f1f0059f1755bd0c11a7a753cb90b80d9f38
João <81783353+JooaoMS@users.noreply.github.com>
2024-05-24 19:37:34 -0300

    exercicio4.py

--- exercicio4.py
+++ exercicio4.py
@@ -1,23 +1,18 @@
-# Número fixo de participantes
 num_participantes = 5
 
-# Listas para armazenar nomes e pontuações
 nomes = []
 pontuacoes = []
 
-# Loop para entrada de dados
 for i in range(num_participantes):
     nome = input("Nome do participante: ")
     pontuacao = float(input("Pontuação do participante: "))
     nomes.append(nome)
     pontuacoes.append(pontuacao)
 
-# Calcula maior pontuação, menor pontuação e pontuação média
 maior_pontuacao = max(pontuacoes)
 menor_pontuacao = min(pontuacoes)
 pontuacao_media = sum(pontuacoes) / num_participantes
 
-# Exibe os resultados
 print("Maior pontuação:", maior_pontuacao)
 print("Menor pontuação:", menor_pontuacao)
 print("Pontuação média:", pontuacao_media)
